0a168b9dd8cc3ee00651429e6591b978698d174d
Rishav Nandi <101431112+rishavnandi@users.noreply.github.com>
2022-10-17 19:56:50 +0530

    Remove Chocolatey as it was not required anymore

--- ps_profile.ps1
+++ ps_profile.ps1
@@ -1,15 +1,5 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
 $ENV:STARSHIP_DISTRO = "者"
 Invoke-Expression (&starship init powershell)
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
 Set-Alias l lsd
 winfetch
