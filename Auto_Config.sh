6d8c5b6bdc3259c4a2c68f31704b906232ad08eb
Rishav Nandi <101431112+rishavnandi@users.noreply.github.com>
2022-10-18 18:00:32 +0530

    Add auto config script

---
+++ Auto_Config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo "
+███████╗████████╗ █████╗ ██████╗ ███████╗██╗  ██╗██╗██████╗     ██████╗ ██████╗  ██████╗ ███╗   ███╗██████╗ ████████╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║  ██║██║██╔══██╗    ██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝
+███████╗   ██║   ███████║██████╔╝███████╗███████║██║██████╔╝    ██████╔╝██████╔╝██║   ██║██╔████╔██║██████╔╝   ██║   
+╚════██║   ██║   ██╔══██║██╔══██╗╚════██║██╔══██║██║██╔═══╝     ██╔═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██╔═══╝    ██║   
+███████║   ██║   ██║  ██║██║  ██║███████║██║  ██║██║██║         ██║     ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║        ██║   
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝         ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝        ╚═╝   
+
+ █████╗ ██╗   ██╗████████╗ ██████╗     ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗                             
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║                             
+███████║██║   ██║   ██║   ██║   ██║    ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║                             
+██╔══██║██║   ██║   ██║   ██║   ██║    ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║                             
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗                        
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝"                        
+
+echo "----------------Setup Bashrc----------------"
+cat bashrc > ~/.bashrc
+dircolors -p | sed 's/;42/;01/' > ~/.dircolors
+
+echo "----------------Setup Starship For Bash----------------"
+curl -sS https://starship.rs/install.sh | sh
+if [ -d "~/.config/" ] 
+then
+    cat starship.toml > ~/.config/starship.toml 
+else
+    mkdir ~/.config/ && cat starship.toml > ~/.config/starship.toml
+fi
+
+echo "----------------Setup PowerShell Profile----------------"
+
+echo "----------------Enter Windows Username----------------"
+read username
+
+if [ -d "/mnt/c/Users/$username/Documents/PowerShell/" ] 
+then
+    cat ps_profile.ps1 > /mnt/c/Users/$username/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+else
+    mkdir /mnt/c/Users/$username/Documents/PowerShell/ && cat ps_profile.ps1 > /mnt/c/Users/$username/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+fi
+
+if [-d "/mnt/c/Users/$username/.starship/"]
+then
+    cat starship.toml > /mnt/c/Users/$username/.starship/starship.toml
+else
+    mkdir /mnt/c/Users/$username/.starship/ && cat starship.toml > /mnt/c/Users/$username/.starship/starship.toml
+fi
+
+if [-d "/mnt/c/Users/$username/.config/winfetch/"]
+then
+    cat winfetch.ps1 > /mnt/c/Users/$username/.config/winfetch/winfetch.ps1
+else
+    mkdir /mnt/c/Users/$username/.config/winfetch/ && cat winfetch.ps1 > /mnt/c/Users/$username/.config/winfetch/winfetch.ps1
+fi
+
+echo "----------------Setup Command Prompt----------------"
+cat starship.lua > /mnt/c/Users/$username/AppData/Local/clink/starship.lua
+
+echo "----------------Setup Windows Terminal----------------"
+terminal_folder=$(ls | grep Terminal)
+cat settings.json > /mnt/c/Users/$username/AppData/Local/Packages/$terminal_folder/LocalState/settings.json
