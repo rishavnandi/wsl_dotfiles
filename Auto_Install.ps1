1c0322c943f924587bc034d21dd0dd3da131033f
Rishav Nandi <101431112+rishavnandi@users.noreply.github.com>
2022-10-21 20:27:20 +0530

    Add apps

--- Auto_Install.ps1
+++ Auto_Install.ps1
@@ -58,6 +58,7 @@ winget install -e --id Microsoft.PowerToys
 scoop install lsd
 scoop install winfetch
 scoop install msiafterburner
+scoop install caffeine
 
 Write-Host "----------------Restore Legacy Context Menu----------------"
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
