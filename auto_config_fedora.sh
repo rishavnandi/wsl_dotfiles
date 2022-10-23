fa3322c3286c39439547d754b408c5697fa2d95f
Rishav Nandi <101431112+rishavnandi@users.noreply.github.com>
2022-10-23 16:01:31 +0530

    Create auto_config_fedora.sh

---
+++ auto_config_fedora.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo "
+███████╗████████╗ █████╗ ██████╗ ███████╗██╗  ██╗██╗██████╗     ██████╗ ██████╗  ██████╗ ███╗   ███╗██████╗ ████████╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║  ██║██║██╔══██╗    ██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝
+███████╗   ██║   ███████║██████╔╝███████╗███████║██║██████╔╝    ██████╔╝██████╔╝██║   ██║██╔████╔██║██████╔╝   ██║
+╚════██║   ██║   ██╔══██║██╔══██╗╚════██║██╔══██║██║██╔═══╝     ██╔═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██╔═══╝    ██║
+███████║   ██║   ██║  ██║██║  ██║███████║██║  ██║██║██║         ██║     ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║        ██║
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝         ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝        ╚═╝
+
+ █████╗ ██╗   ██╗████████╗ ██████╗     ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
+███████║██║   ██║   ██║   ██║   ██║    ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║
+██╔══██║██║   ██║   ██║   ██║   ██║    ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝"
+
+echo "----------------https://github.com/rishavnandi/Dotfiles----------------"
+
+echo "----------------Enter Unix Username----------------"
+read user
+
+echo "----------------Setup Common Software----------------"
+sudo dnf update -y
+sudo dnf install lsd neovim neofetch git curl wget unzip ranger feh cmatrix lynx fzf speedtest-cli fish dos2unix -y
+
+echo "----------------Setup Bashrc----------------"
+sudo cat bashrc > /home/$user/.bashrc
+sudo dos2unix /home/$user/.bashrc
+
+echo "----------------Disable Directory Highlights----------------"
+dircolors -p | sed 's/;42/;01/' > /home/$user/.dircolors
+
+echo "----------------Setup Starship For Bash----------------"
+curl -sS https://starship.rs/install.sh | sh
+if [ -d "/home/$user/.config/" ]
+then
+    sudo cat linux_starship.toml > /home/$user/.config/starship.toml
+else
+    sudo mkdir /home/$user/.config/ && sudo cat linux_starship.toml > /home/$user/.config/starship.toml
+fi
+
+echo "----------------Setup Fish Config----------------"
+if [ -d "/home/$user/.config/fish/" ]
+then
+    sudo cat fish_config > /home/$user/.config/fish/config.fish
+else
+    sudo mkdir /home/$user/.config/fish/ && sudo cat fish_config > /home/$user/.config/fish/config.fish
+fi
+
+echo "----------------Change Default Shell To Fish----------------"
+chsh -s /usr/bin/fish
+
+
+echo "----------------Setup Vagrant For Fedora----------------"
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+sudo dnf -y install vagrant
+vagrant plugin install virtualbox_WSL2
+vagrant plugin install vagrant-vbguest
+vagrant autocomplete install --bash
+
+echo "----------------Setup Terraform For Fedora----------------"
+sudo dnf -y install terraform
+terraform -install-autocomplete
+
+echo "----------------Setup pip----------------"
+sudo dnf install python3-pip -y
+
+echo "----------------Setup Ansible----------------"
+pip3 install ansible
+pip3 install ansible-lint
+
+echo "----------------Enable hushlogin----------------"
+touch /home/$user/.hushlogin
+
+echo "----------------Setup Git----------------"
+echo "----------------Enter First Name----------------"
+read first
+echo "----------------Enter Last Name----------------"
+read last
+echo "----------------Enter GitHub Email----------------"
+read gitemail
+git config --global user.name "$first $last"
+git config --global user.email "$gitemail"
